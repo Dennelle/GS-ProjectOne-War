60d5fbd649ae157d024f400da36dcd3638a0ed87
Dennelle <dennelle.walton@gmail.com>
2023-10-08 18:40:46 -0400

    add separate play buttons

--- js/script.js
+++ js/script.js
@@ -1,43 +1,70 @@
 /*----- constants -----*/
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function shuffleCards(arr){
-    return arr.sort( () => Math.random() - 0.5 );
-}
+
 
 /*----- state variables: what information does the application need to remember throughout its execution-----*/
 let playerOneDeck = []
 let playerTwoDeck = []
 
 
+
 /*----- cached elements -----*/
 const shuffleEl = document.querySelector('#shuffle');
 const messageEl = document.querySelector('h1');
-const playCardBtnEl = document.querySelector('#playCards');
+const dealCardsBtnEl = document.querySelector('#dealCards');
 const num1El = document.querySelector('#number1');
 const num2El = document.querySelector('#number2');
 const playerOneNum = document.querySelector('#number1')
 const playerTwoNum = document.querySelector('#number2')
-
-/*----- event listeners -----*/
-playCardBtnEl.addEventListener('click', () => {
-    divideDeck()
-});
-
-shuffleEl.addEventListener('click', () => {
-    shuffleCards(cards)
-    console.log(shuffleCards(cards))
-});
-
+const playOneBtnEl = document.querySelector('#playButtonOne')
+const playTwoBtnEl = document.querySelector('#playButtonTwo')
 
 /*----- functions -----*/
+function shuffleCards(arr) {
+    return arr.sort( () => Math.random() - 0.5 );
+}
 function divideDeck() {
     playerOneCards = cards.slice(0,5)
     console.log(playerOneCards)
 
     playerTwoCards = cards.slice(5,10)
     console.log(playerTwoCards)
-    }
+}
+// thinking how to compare the numbers
+function playerOne() {
+    playerOneCards = cards.slice(0,5)
+    console.log(playerOneCards)
+}
+
+function playerTwo() {
+    playerTwoCards = cards.slice(5,10)
+    console.log(playerTwoCards)
+}
+
+//function to deal cards to player one and player two.
+
+
+/*----- event listeners -----*/
+shuffleEl.addEventListener('click', () => {
+    shuffleCards(cards)
+    console.log(shuffleCards(cards))
+});
+
+dealCardsBtnEl.addEventListener('click', () => {
+    divideDeck()
+});
+
+//thinking how to compare the arrays
+playOneBtnEl.addEventListener('click', () => {
+    playerOne()
+});
+
+playTwoBtnEl.addEventListener('click', () => {
+    playerTwo()
+});
+
+// initialize all state, then call render()
 
 render()
 
@@ -45,7 +72,7 @@ render()
 init();
 
 
-// initialize all state, then call render()
+
 function init(){
 
     render();
