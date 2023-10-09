3d4acda58a072d4d3269b61128bc696fcec776c1
Dennelle <dennelle.walton@gmail.com>
2023-10-09 16:58:30 -0400

    checking git was changged

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', '
 
 
 
+
+
+
 /*----- state variables: what information does the application need to remember throughout its execution-----*/
 let playerOneDeck = []
 let playerTwoDeck = []
 let score;
+
 /*----- cached elements -----*/
 const shuffleEl = document.querySelector('#shuffle');
 const messageEl = document.querySelector('h1');
@@ -19,33 +23,32 @@ const playerOneNum = document.querySelector('#number1')
 const playerTwoNum = document.querySelector('#number2')
 const playOneBtnEl = document.querySelector('#playButtonOne')
 const playTwoBtnEl = document.querySelector('#playButtonTwo')
+let i = 0
 
 // Build an 'original' deck of 'card' objects used to create shuffled decks
 const originalDeck = buildOriginalDeck();
 const shuffledContainer = document.getElementById('shuffled-deck-container');
 const playCard = document.querySelector(".playCard");
 
-
-
 /*----- functions -----*/
     function buildOriginalDeck()
     {
     // Use nested forEach to generate card objects
         suits.forEach(function(suit)
-        {
-        ranks.forEach(function(rank)
-        {
-        deck.push
-        (
     {
+        ranks.forEach(function(rank)
+    {
+        deck.push
+    (
+        {
     // The 'face' property maps to the library's CSS classes for cards
-        face: `${suit}${rank}`,
-        value: Number(rank) || (rank === 'A' ? 11 : 10)
+    face: `${suit}${rank}`,
+    value: Number(rank) || (rank === 'A' ? 11 : 10)
     }
-        );
-    }   );
-    }   );
-        return deck;
+    );
+    } );
+    } );
+    return deck;
     }
 
     function shuffleCards(arr)
@@ -62,74 +65,67 @@ const playCard = document.querySelector(".playCard");
     console.log(playerTwoDeck);
     }
 
-
-    // function compareHands()
-    // {
-    // if (playerOneDeck[0].value > playerTwoDeck[0].value)
-    //     console.log (' Player One Wins!')
-    // else if
-    //     (playerOneDeck[0].value < playerTwoDeck[0].value)
-    //   console.log (' Player Two Wins!')
-    // else
-    //     ('It\'s a tie!')
-    // }
-
-
     function compareHands()
     {
-    // if (playerOneDeck[0].value > playerTwoDeck[0].value)
-    //     console.log (' Player One Wins!')
-    // else if
-    //     (playerOneDeck[0].value < playerTwoDeck[0].value)
-    //   console.log (' Player Two Wins!')
-    // else
-    //     ('It\'s a tie!')
-
-    let i = 0;
-    while (i < playerTwoDeck.length) {
-        if (playerOneDeck[i].value > playerTwoDeck[i].value)
-        console.log (' Player One Wins!')
-        else if
-        (playerOneDeck[i].value < playerTwoDeck[i].value)
-        console.log (' Player Two Wins!')
-         else
-        ('It\'s a tie!')
-        i++;
+    //add the brackets
+    if (playerOneDeck[i].value > playerTwoDeck[i].value)
+    console.log (' Player One Wins!')
+    else if
+    (playerOneDeck[i].value < playerTwoDeck[i].value)
+    console.log (' Player Two Wins!')
+    else
+    ('It\'s a tie!')
     }
-    }
-
 
 /*----- event listeners -----*/
-shuffleEl.addEventListener('click', () => {
+    shuffleEl.addEventListener('click', () => {
     shuffleCards(deck)
     console.log(shuffleCards(deck))
-});
+    });
 
-dealCardsBtnEl.addEventListener('click', () => {
+    dealCardsBtnEl.addEventListener('click', () => {
     divideDeck()
-});
+    });
 
-playOneBtnEl.addEventListener('click', () => {
+    playOneBtnEl.addEventListener('click', () => {
     compareHands()
-});
+    i += 1; i < 27;
+    });
+
+
 
 
 // initialize all state, then call render()
 
+
 render()
+
+
 
 
 init();
 
 
 
+
+
+
 function init(){
 
-    render();
+
+render();
 }
+
 
 function render() {
 }
+
+
+
+
+
+
+
 
 
 
