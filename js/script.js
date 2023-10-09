47379b0b294d973bc31c740618ffba7947e2142c
Dennelle <dennelle.walton@gmail.com>
2023-10-09 14:49:29 -0400

    compareHands function works for one index. q cd

--- js/script.js
+++ js/script.js
@@ -3,28 +3,12 @@ const deck = [];
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
-function buildOriginalDeck() {
-    //const deck = [];
-    // Use nested forEach to generate card objects
-    suits.forEach(function(suit) {
-    ranks.forEach(function(rank) {
-    deck.push({
-    // The 'face' property maps to the library's CSS classes for cards
-    face: `${suit}${rank}`,
-    value: Number(rank) || (rank === 'A' ? 11 : 10)
-    });
-    });
-    });
-    return deck;
-    }
+
 
 /*----- state variables: what information does the application need to remember throughout its execution-----*/
 let playerOneDeck = []
 let playerTwoDeck = []
-
-let shuffledDeck;
-
-
+let score;
 /*----- cached elements -----*/
 const shuffleEl = document.querySelector('#shuffle');
 const messageEl = document.querySelector('h1');
@@ -44,47 +28,60 @@ const playCard = document.querySelector(".playCard");
 
 
 /*----- functions -----*/
-    function shuffleCards(arr) {
+    function buildOriginalDeck()
+    {
+    // Use nested forEach to generate card objects
+        suits.forEach(function(suit)
+        {
+        ranks.forEach(function(rank)
+        {
+        deck.push
+        (
+    {
+    // The 'face' property maps to the library's CSS classes for cards
+        face: `${suit}${rank}`,
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+    }
+        );
+    }   );
+    }   );
+        return deck;
+    }
+
+    function shuffleCards(arr)
+    {
     return arr.sort( () => Math.random() - 0.5 );
     }
 
-    function divideDeck() {
-    playerOneDeck = deck.slice(0,26)
+    function divideDeck()
+    {
+    playerOneDeck = deck.slice(0,26);
+    playerOneDeck.push
     console.log(playerOneDeck);
 
-    playerTwoDeck = deck.slice(26,52)
+    playerTwoDeck = deck.slice(26,52);
+    playerTwoDeck.push
     console.log(playerTwoDeck);
-}
-
-    function getNewShuffledDeck() {
-    // Create a copy of the originalDeck (leave originalDeck untouched!)
-    const tempDeck = [...originalDeck];
-    const newShuffledDeck = [];
-    while (tempDeck.length) {
-    // Get a random index for a card still in the tempDeck
-    const rndIdx = Math.floor(Math.random() * tempDeck.length);
-    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
     }
-    return newShuffledDeck;
+//     {
+//     if (playerOneDeck[0].value > playerTwoDeck[0].value)
+//         console.log (' Player One Wins!')
+//     else if
+//          (playerOneDeck[0].value < playerTwoDeck[0].value)
+//       console.log (' Player Two Wins!')
+//     else ('It\'s a tie!')
+//     }
+
+    function compareHands()
+    {
+    if (playerOneDeck[0].value > playerTwoDeck[0].value)
+        console.log (' Player One Wins!')
+    else if
+        (playerOneDeck[0].value < playerTwoDeck[0].value)
+      console.log (' Player Two Wins!')
+    else
+        ('It\'s a tie!')
     }
-
-    function handleMove(evt) {
-        // Check if we should exit the function
-        // according to the current state
-        // For example, ignore clicks if the
-        // game has been won or is a tie:
-        if (winner) return;
-        // Logic/Code to update all impacted state
-        // Visualize all state
-        render();
-        }
-    function renderNewShuffledDeck() {
-            // Create a copy of the originalDeck (leave originalDeck untouched!)
-            shuffledDeck = getNewShuffledDeck();
-            }
-
-
 
 /*----- event listeners -----*/
 shuffleEl.addEventListener('click', () => {
@@ -96,8 +93,10 @@ dealCardsBtnEl.addEventListener('click', () => {
     divideDeck()
 });
 
+playOneBtnEl.addEventListener('click', () => {
+    compareHands()
+});
 
-document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
 
 // initialize all state, then call render()
 
@@ -118,8 +117,6 @@ function render() {
 
 
 
-
-renderNewShuffledDeck();
 
 
 
